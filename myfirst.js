62c9d774df2623f29740da367eb06ff9e4860b58
obadacharif <obadaalsharif3@gmail.com>
2021-11-20 14:38:27 +0100

    start tuto

--- myfirst.js
+++ myfirst.js
@@ -1,12 +1,11 @@
-/*
 var http = require('http');
 
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write(req.url);
+  res.write( "hello world ! : " +  req.url);
   res.end();
-}).listen(8080);*/
+}).listen(8080);
 
 /*
 var url = require('url');
@@ -66,62 +65,76 @@ http.createServer(function (req, res) {
 }).listen(8080);
 */
 
-var http = require('http');
-var url = require('url');
-var fs = require('fs');
+/*
+var http = require("http");
+var url = require("url");
+var fs = require("fs");
 
 var users = [
   {
     username: "mohamed",
-    age: 19
+    age: 19,
   },
   {
     username: "asma",
-    age: 25
+    age: 25,
   },
   {
     username: "abdelhak",
-    age: 39
+    age: 39,
   },
 ];
 
 var products = [
   {
     name: "DELL LATITUDE",
-    price: 170000
+    price: 170000,
   },
   {
     name: "MACBOOK PRO",
-    price: 250000
+    price: 250000,
   },
   {
     name: "HP LITEBOOK",
-    price: 145000
+    price: 145000,
   },
 ];
 
+http
+  .createServer(function (req, res) {
+    var q = url.parse(req.url, true);
+    
+    console.log("LOG: " + req.url);
 
-http.createServer(function (req, res) {
-  
-  var q = url.parse(req.url, true);
-  console.log({body:req.body});
+    if (q.pathname === "/api/users") {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.write(JSON.stringify(users));
+      return res.end();
+    }
 
-  if(q.pathname === "/api/users"){
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    res.write(JSON.stringify(users));
-    return res.end();
-  }
+    if (q.pathname === "/api/products") {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.write(JSON.stringify(products));
+      return res.end();
+    }
 
-  if(q.pathname === "/api/products"){
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    res.write(JSON.stringify(products));
-    return res.end();
-  }
+    if(q.pathname === "/api/add_user"){
+      console.log("q.username : "  + q.query.username);
+      users.push({
+        username: q.query.username,
+        age: q.query.age
+      });
+    }
 
-
-  res.writeHead(200, {'Content-Type': 'application/json'});
-  res.write("ERROR");
-  return res.end();
-  
-}).listen(8080);
-
+    var filename = "." + q.pathname;
+    fs.readFile(filename, function (err, data) {
+      if (err) {
+        res.writeHead(404, { "Content-Type": "text/html" });
+        return res.end("404 Not Found");
+      }
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      return res.end();
+    });
+  })
+  .listen(8080);*/
